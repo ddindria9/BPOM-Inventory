667bb80e4927674fa730b4b0888026bcc2e34bf1
ddindria9 <146157759+ddindria9@users.noreply.github.com>
2026-06-29 09:53:11 +0700

    Update default value for jabatan in SPBIn model
    Change default value of 'jabatan' to 'staff'.

--- backend/server.py
+++ backend/server.py
@@ -513,7 +513,7 @@ class SPBIn(BaseModel):
     nama_pegawai: str
     nip_pegawai: str = "" 
     unit_kerja: str
-    jabatan: str = ""   # staff / kepala_fungsi
+    jabatan: str = "staff"   default
     keperluan: str = ""
     lines: List[SPBLine]
 
